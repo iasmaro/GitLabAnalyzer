f4d3c1b0de371ca3d08622bcf5124bb3dbec7061
Andrew Ursu <aursu@sfu.ca>
2021-01-31 13:00:09 -0800

    Committing CommitWrapper model file

---
+++ backend/src/main/java/com/haumea/gitanalyzer/model/CommitWrapper.java
@@ -0,0 +1,40 @@
+package com.haumea.gitanalyzer.model;
+import org.gitlab4j.api.CommitsApi;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Commit;
+import org.gitlab4j.api.models.Diff;
+import org.gitlab4j.api.models.Project;
+
+import java.util.List;
+/*
+Wrapper class designed to encapsulate the commit code from the gitlab library
+
+Needed as the commit data and code differences are separated into 2 different objects
+
+ */
+
+
+
+public class CommitWrapper {
+
+    private final Commit commitData;
+    private final List<Diff> newCode;
+
+    public List<Diff> getNewCode() { return newCode; }
+
+    public Commit getCommitData() {
+        return commitData;
+    }
+
+    // need to create commitData list in calling code and create the student and commit wrapper objects from that list
+    public CommitWrapper(GitLabApi gitLabApi, Project project, Commit commitData) throws GitLabApiException {
+        CommitsApi commitApi = new CommitsApi(gitLabApi);
+
+        this.commitData = commitData;
+        this.newCode = commitApi.getDiff(project, commitData.getId());
+
+
+    }
+}
+
