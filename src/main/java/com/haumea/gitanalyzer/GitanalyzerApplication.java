f26fea55cf4eb5f9c58b3790cae7b44287ffb0ab
tmbui <tmbui@sfu.ca>
2021-01-30 18:59:12 -0800

    remove unneeded imports

--- src/main/java/com/haumea/gitanalyzer/GitanalyzerApplication.java
+++ src/main/java/com/haumea/gitanalyzer/GitanalyzerApplication.java
@@ -1,9 +1,7 @@
 package com.haumea.gitanalyzer;
 
-import com.haumea.gitanalyzer.student.StudentRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class GitanalyzerApplication {
