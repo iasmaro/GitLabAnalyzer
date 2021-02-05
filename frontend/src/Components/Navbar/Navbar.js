20b8676aba8d8f64c7a7b112d5f2475dcf3fe8d8
Sterling Tamboline <stamboli@sfu.ca>
2021-02-05 14:57:52 -0800

    Added navbar and style

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import './Navbar.css';
 
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <nav className="nav-bar">
+                <h1 className="nav-bar-logo">GitLab Analyzer</h1>
 
-const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
+                <ul className="nav-bar-links">
+                    <li> 
+                        <a href="#">Sign in</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                </ul>
+            </nav>
+        )  
+    }
 }
+
 
 export default Navbar
