36428b5d81616b0a6a155856e140c9aa06946267
Austin Wang <ajwang@sfu.ca>
2021-02-09 16:36:39 -0800

    Configure Swagger
    Add ApiInfo to swagger-ui

--- backend/src/main/java/com/haumea/gitanalyzer/config/SwaggerConfig.java
+++ backend/src/main/java/com/haumea/gitanalyzer/config/SwaggerConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +19,18 @@ public class SwaggerConfig {
                 .select()
                 .paths(PathSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.haumea"))
-                .build();
+                .build()
+                .apiInfo(apiDetails());
     }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "GitLabAnalyzer API",
+                "Documentation for GitLabAnalyzer API.",
+                "1.0",
+                "Terms of service",
+                new springfox.documentation.service.Contact("Haumea", "www.sfu.ca", "Haumea@sfu.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+
 }
