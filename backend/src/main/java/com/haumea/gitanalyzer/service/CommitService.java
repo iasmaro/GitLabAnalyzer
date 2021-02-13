ba208834e1b0aa6c18eefd171469d25feb5ad317
Austin Wang <ajwang@sfu.ca>
2021-02-12 22:58:35 -0800

    wip

--- backend/src/main/java/com/haumea/gitanalyzer/service/CommitService.java
+++ backend/src/main/java/com/haumea/gitanalyzer/service/CommitService.java
@@ -2,6 +2,7 @@ package com.haumea.gitanalyzer.service;
 
 import com.haumea.gitanalyzer.gitlab.GitlabService;
 import com.haumea.gitanalyzer.model.User;
+import jdk.nashorn.internal.runtime.GlobalConstants;
 import org.gitlab4j.api.models.Commit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,23 @@ import java.util.List;
 
 @Service
 public class CommitService {
-    private final GitlabService gitLabService;
+    private final UserService userService;
 
     @Autowired
-    public CommitService(GitlabService gitLabService) {
-        this.gitLabService = gitLabService;
+    public CommitService(UserService userService) {
+        this.userService = userService;
     }
 
     public List<Commit> getMergeRequestCommitsForMember(int projectId, int mergeRequestId, String memberId) throws Exception{
+        String token;
+        try {
+            token = userService.getPersonalAccessToken(memberRequestDTO.getUserId());
+        }
+        catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+
+        GitlabService gitlabService = new GitlabService(GlobalConstants.gitlabURL, token);
         try {
             return gitLabService.getMergeRequestCommitsForMember(projectId, mergeRequestId, memberId);
         }
