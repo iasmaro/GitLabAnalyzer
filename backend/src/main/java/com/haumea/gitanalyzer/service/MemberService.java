d1b634d14f7caefe595767a8927f697c1eac6ad3
tmbui <tmbui@sfu.ca>
2021-02-12 22:23:15 -0800

    switch to using Gitlab service
    change memberId type from int to String, change getId to getUsername

--- backend/src/main/java/com/haumea/gitanalyzer/service/MemberService.java
+++ backend/src/main/java/com/haumea/gitanalyzer/service/MemberService.java
@@ -2,10 +2,10 @@ package com.haumea.gitanalyzer.service;
 
 import com.haumea.gitanalyzer.dao.MemberRepository;
 import com.haumea.gitanalyzer.dto.MemberRequestDTO;
+import com.haumea.gitanalyzer.gitlab.GitlabService;
+import com.haumea.gitanalyzer.gitlab.MemberWrapper;
 import com.haumea.gitanalyzer.utility.GlobalConstants;
-import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
-import org.gitlab4j.api.ProjectApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,14 +39,13 @@ public class MemberService {
             throw new Exception(e.getMessage());
         }
 
-        GitLabApi gitLabApi = new GitLabApi(GlobalConstants.gitlabURL, token);
+        GitlabService gitlabService = new GitlabService(GlobalConstants.gitlabURL, token);
 
-        ProjectApi projectApi = new ProjectApi(gitLabApi);
         List<String> members = new ArrayList<>();
         try {
-            List<org.gitlab4j.api.models.Member> gitlabMembers = projectApi.getAllMembers(memberRequestDTO.getProjectId());
-            for(org.gitlab4j.api.models.Member current: gitlabMembers){
-                members.add(current.getUsername());
+            List<MemberWrapper> gitlabMembers = gitlabService.getMembers(memberRequestDTO.getProjectId());
+            for(MemberWrapper current: gitlabMembers){
+                members.add(current.getMemberId());
             }
 
             return members;
