04d1d56d0c27fc3fb4f727d66d07b621563258b5
tmbui <tmbui@sfu.ca>
2021-02-12 22:00:19 -0800

    add set-up code for DTO mapping

--- backend/src/main/java/com/haumea/gitanalyzer/dto/MemberResponseDTO.java
+++ backend/src/main/java/com/haumea/gitanalyzer/dto/MemberResponseDTO.java
@@ -1,4 +1,13 @@
 package com.haumea.gitanalyzer.dto;
 
+import java.util.List;
+
 public class MemberResponseDTO {
+    private String memberId;
+    private List<String> alias;
+
+    public MemberResponseDTO(String memberId, List<String> alias) {
+        this.memberId = memberId;
+        this.alias = alias;
+    }
 }
---
+++ backend/src/main/java/com/haumea/gitanalyzer/mapper/MemeberMapper.java
@@ -0,0 +1,9 @@
+package com.haumea.gitanalyzer.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface MemeberMapper {
+
+}
