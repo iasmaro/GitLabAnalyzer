d0200ef5ee59b3c02e5c5dd0281e49160a61aad4
Sterling Tamboline <stamboli@sfu.ca>
2021-02-15 12:48:16 -0800

    Removed login page and form because sfu sso is working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
-import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home'
 import About from './Pages/About/About';
 import Profile from './Pages/Profile/Profile';
 import Dashboard from './Pages/Dashboard/Dashboard';
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
 
       <Navbar />
-      <Route exact path="/" component={Login}></Route>
+      <Route exact path="/" component={Home}></Route>
       <Route exact path="/about" component={About}></Route>
       <Route exact path="/profile" component={Profile}></Route>
       <Route exact path="/dashboard" component={Dashboard}/>
