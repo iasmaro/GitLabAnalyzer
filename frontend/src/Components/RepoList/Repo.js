2cd1d9ab70834202c3334a123b76128c713aed02
Neil Shah <nmshah@sfu.ca>
2021-02-16 01:40:00 -0800

    Changed button components to react bootstrap components

--- frontend/src/Components/RepoList/Repo.js
+++ frontend/src/Components/RepoList/Repo.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 const Repo = (props) => {
     return (
         <tr>
             <td>{props.repo.name}</td>
             <td>{props.repo.date}</td>
-            <button>Analyze</button>
+            <td>
+                <Button variant="light" className= 'Analyze' >Analyze</Button>{' '}
+            </td>
         </tr>
     );
 };
