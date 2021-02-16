5d8a2e1381469886bc686be766c3099000a5fec3
Neil Shah <nmshah@sfu.ca>
2021-02-16 01:51:13 -0800

    Made routing changes and added tab component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
 
 import About from 'Pages/About/About';
 import Home from 'Pages/Home/Home';
+import Profile from 'Pages/Profile/Profile';
 import Navbar from 'Components/Navbar/Navbar';
 import getUserData from 'Utils/getUserData';
 
 import { useUserDispatch } from './UserContext';
 import ProtectedRoute from './ProtectedRoute';
 import LogoutPage from './LogoutPage';
+import AnalyzerTabs from 'Components/AnalyzerTabs/AnalyzerTabs';
 
 
 function App() {
@@ -30,12 +32,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <AnalyzerTabs />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <ProtectedRoute exact path="/Configurations" Component={Home} />
         <ProtectedRoute exact path="/Reports" Component={Home} />
-        <ProtectedRoute exact path="/Profile" Component={Home} />
+        <ProtectedRoute exact path="/Profile" Component={Profile} />
         <LogoutPage exact path="/Logout" />
       </Switch>
     </div>
