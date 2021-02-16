bbf1d4fe4cf1407fe55c6a7511ba3beb6b6fd3b3
Ivan <ivan_asmaro@sfu.ca>
2021-02-15 22:33:08 -0800

    Add API calls for repos and access token

--- frontend/src/Components/RepoList/RepoList.js
+++ frontend/src/Components/RepoList/RepoList.js
@@ -4,25 +4,35 @@ import Table from 'react-bootstrap/Table';
 import Repo from './Repo';
 import './RepoList.css';
 
-const RepoList = ({repos}) => {
-        return (
-            <div className = 'list-container'>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Repo Name</th>
-                            <th>Last Modified</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {repos.map((repo) => (
-                            <Repo key={repo.id} repo={repo}/>
-                        ))}
-                    </tbody>
-                </Table>
-            </div>
-        );
-    };
+const RepoList = (props) => {
+    const { repos } = props || {};
+    const tokensNotSetMessage = 'Please set your gitlab token in the Profile page';
+    const noReposMessage = 'It seems that you do not have any repositories at this moment';
+    return (
+        <div className = 'list-container'>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Repo Name</th>
+                        <th>Last Modified</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!repos ? (
+                        <td colSpan={3} >{tokensNotSetMessage}</td>
+                    )
+                    : !repos.length ? (
+                        <td colSpan={3} >{noReposMessage}</td>
+                    )
+                    :
+                    repos.map((repo) => (
+                        <Repo key={repo?.projectId} repo={repo}/>
+                    ))}
+                </tbody>
+            </Table>
+        </div>
+    );
+};
 
 export default RepoList
--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -1,11 +1,15 @@
 const dev = {
     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://localhost:3000/',
-    SFU_AUTHENTICATION_URL: 'https://cas.sfu.ca/cas/serviceValidate?service=http://localhost:3000/'
+    SFU_AUTHENTICATION_URL: 'https://cas.sfu.ca/cas/serviceValidate?service=http://localhost:3000/',
+    REPOS_API_URL: 'http://localhost:8080/api/v1/projects',
+    USERS_API_URL: 'http://localhost:8080/api/v1/users',
 }
 
 const prod = {
     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://cmpt373-1211-11.cmpt.sfu.ca/',
-    SFU_AUTHENTICATION_URL: 'https://cas.sfu.ca/cas/serviceValidate?service=http://cmpt373-1211-11.cmpt.sfu.ca/'
+    SFU_AUTHENTICATION_URL: 'https://cas.sfu.ca/cas/serviceValidate?service=http://cmpt373-1211-11.cmpt.sfu.ca/',
+    REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca/api/v1/projects',
+    USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca/api/v1/users',
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
