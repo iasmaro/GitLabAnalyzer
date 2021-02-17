bb0b014d3feaa0e2c7a11b296b8cb469a2866521
Austin Wang <ajwang@sfu.ca>
2021-02-16 21:45:14 -0800

    test using sfu gitlab server

--- backend/src/main/java/com/haumea/gitanalyzer/utility/GlobalConstants.java
+++ backend/src/main/java/com/haumea/gitanalyzer/utility/GlobalConstants.java
@@ -1,5 +1,6 @@
 package com.haumea.gitanalyzer.utility;
 
 public final class GlobalConstants {
-    public static final String gitlabURL = "http://cmpt373-1211-11.cmpt.sfu.ca/gitlab";
+//    public static final String gitlabURL = "http://cmpt373-1211-11.cmpt.sfu.ca/gitlab";
+    public static final String gitlabURL = "https://csil-git1.cs.surrey.sfu.ca";
 }
