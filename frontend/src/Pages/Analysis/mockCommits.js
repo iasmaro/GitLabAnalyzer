9a0bc5d900fe9c3be29bcdc6b57844393a904b4c
Yi Ching Chang <changyic@sfu.ca>
2021-02-17 15:04:34 +0800

    Update mock commit dates

--- frontend/src/Pages/Analysis/mockCommits.js
+++ frontend/src/Pages/Analysis/mockCommits.js
@@ -2,67 +2,67 @@ export const commits = [
     {
         id: 'sdf31sc',
         message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        date: 'May 11',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: '3flsidv',
         message: 'Volutpat sed cras ornare arcu dui vivamus.',
-        date: 'Sep 2',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: '4lsdif8',
         message: 'Urna et pharetra pharetra massa massa.',
-        date: 'Mar 18',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: 's0dsfk4',
         message: 'Eu ultrices vitae auctor eu augue ut lectus arcu bibendum.',
-        date: 'Feb 5',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: 'd2f31fc',
         message: 'Elementum facilisis leo vel fringilla est ullamcorper eget.',
-        date: 'Sep 23',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: 'lsk35kd',
         message: 'Quisque egestas diam in arcu cursus euismod quis viverra nibh',
-        date: 'Oct 31',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: '2lkfi53',
         message: 'Urna cursus eget nunc scelerisque. Nisl vel pretium lectus quam',
-        date: 'Apr 20',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: '9sd0fki',
         message: 'Euismod nisi porta lorem mollis aliquam ut porttitor eget aliquet nibh praesent.',
-        date: 'Jun 3',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: '5lsifkd',
         message: 'Elit at imperdiet dui accumsan sit amet nulla facilisi',
-        date: 'Oct 31',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: 'x9fq33l',
         message: 'Tortor aliquam nulla facilisi cras fermentum odio',
-        date: 'Apr 20',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     },
     {
         id: 'x9fq33l',
         message: 'Euismod nisi porta lorem mollis aliquam ut porttitor eget aliquet nibh praesent. Tortor aliquam nulla facilisi cras fermentum odio.',
-        date: 'Apr 20',
+        date: '2021-02-17T06:49:59.563Z',
         author: 'johndoe'
     }
 ]
