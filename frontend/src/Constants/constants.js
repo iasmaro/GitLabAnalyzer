fe7382737c7bd95977926e7ee2ffb6d972f452da
Ivan <ivan_asmaro@sfu.ca>
2021-02-16 18:47:11 -0800

    Add API call to get project members

--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -3,6 +3,7 @@ const dev = {
     SFU_AUTHENTICATION_URL: 'https://cas.sfu.ca/cas/serviceValidate?service=http://localhost:3000/',
     REPOS_API_URL: 'http://localhost:8080/api/v1/projects',
     USERS_API_URL: 'http://localhost:8080/api/v1/users',
+    PROJECT_MEMBERS_API_URL: 'http://localhost:8080/api/v1/members',
 }
 
 const prod = {
@@ -10,6 +11,7 @@ const prod = {
     SFU_AUTHENTICATION_URL: 'https://cas.sfu.ca/cas/serviceValidate?service=http://cmpt373-1211-11.cmpt.sfu.ca/',
     REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca/api/v1/projects',
     USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca/api/v1/users',
+    PROJECT_MEMBERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca/api/v1/members',
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
