566b901af715be1b4df9d9512706f08d4a737142
Neil Shah <nmshah@sfu.ca>
2021-02-20 19:43:19 -0800

    Adding updated constants file

--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -1,9 +1,25 @@
+// const dev = {
+//     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://localhost:3000/',
+//     AUTHENTICATION_API_URL: 'http://localhost:8080/api/v1/users/userId?url=http://localhost:3000/',
+//     REPOS_API_URL: 'http://localhost:8080/api/v1/projects',
+//     USERS_API_URL: 'http://localhost:8080/api/v1/users',
+//     PROJECT_MEMBERS_API_URL: 'http://localhost:8080/api/v1/members',
+// }
+
+// const prod = {
+//     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://cmpt373-1211-11.cmpt.sfu.ca/',
+//     AUTHENTICATION_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/userId?url=http://cmpt373-1211-11.cmpt.sfu.ca/',
+//     REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/projects',
+//     USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users',
+//     PROJECT_MEMBERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/members',
+// }
+
 const dev = {
-    SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://localhost:3000/',
-    AUTHENTICATION_API_URL: 'http://localhost:8080/api/v1/users/userId?url=http://localhost:3000/',
-    REPOS_API_URL: 'http://localhost:8080/api/v1/projects',
-    USERS_API_URL: 'http://localhost:8080/api/v1/users',
-    PROJECT_MEMBERS_API_URL: 'http://localhost:8080/api/v1/members',
+    SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://cmpt373-1211-11.cmpt.sfu.ca/',
+    AUTHENTICATION_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/userId?url=http://cmpt373-1211-11.cmpt.sfu.ca/',
+    REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/projects',
+    USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users',
+    PROJECT_MEMBERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/members',
 }
 
 const prod = {
@@ -13,6 +29,7 @@ const prod = {
     USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users',
     PROJECT_MEMBERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/members',
 }
+
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
 
@@ -30,6 +47,7 @@ export const TABS = {
     COMMITS: 'Commits'
 }
 export const message = {
+    NO_MERGE_REQUEST: 'No merge requests found for this user',
     NO_COMMITS: 'No commits found for this user',
     TOKEN_NOT_SET: 'Please set your gitlab token in the Profile page',
     NO_REPOS: 'It seems that you do not have any repositories at this moment',
