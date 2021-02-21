dee1b1b41f15fc6e7473e8714f66f75bf0afc63d
Yi Ching Chang <changyic@sfu.ca>
2021-02-21 13:43:36 +0800

    Remove processDate file

--- frontend/src/Components/RepoModal/Utils/processDates.js
+++
@@ -1,9 +0,0 @@
-export function utcToLocal(utcString){
-    var localDate = new Date(utcString);
-    return localDate;
-}
-
-export function localToUtc(year, month, date, hour, minute, second){
-    var localDate = new Date(year, month-1, date, hour, minute, second);
-    return localDate.toISOString();
-}
