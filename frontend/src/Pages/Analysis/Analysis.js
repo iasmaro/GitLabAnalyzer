29dbe406b775343257fbde4fc962e74094480cc4
Neil Shah <nmshah@sfu.ca>
2021-02-20 19:43:47 -0800

    Adding updated analysis file

--- frontend/src/Pages/Analysis/Analysis.js
+++ frontend/src/Pages/Analysis/Analysis.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import CommitsList from 'Components/CommitsList/CommitsList';
+import MergeRequestList from 'Components/MergeRequestList/MergeRequestList';
 
 import { commits } from 'Mocks/mockCommits';
+import { mergerequests } from 'Mocks/mockMergeRequests'
 
 const Analysis = (props) => {
     return (
         <div>
-            <CommitsList commits={commits}/>
+            <MergeRequestList mergerequests={mergerequests}/>
         </div>
     )
 }
 
 export default Analysis;
+
