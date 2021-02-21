230e5bcee4a0f13884e54f372f87440831a84688
Neil Shah <nmshah@sfu.ca>
2021-02-20 19:44:49 -0800

    Adding temporary merge request data

---
+++ frontend/src/Mocks/mockMergeRequests.js
@@ -0,0 +1,35 @@
+export const mergerequestEmpty = [];
+export const mergerequests = [
+    {
+        createdDate: "2021-02-21T02:48:26.981Z",
+        memberScore: 0,
+        mergeId: "Neil",
+        mergedDate: "2021-02-21T02:48:26.981Z",
+        mrscore: 0,
+        updatedDate: "2021-02-21T02:48:26.981Z"
+    },
+    {
+        createdDate: "2021-02-21T02:48:26.981Z",
+        memberScore: 0,
+        mergeId: "Tom",
+        mergedDate: "2021-02-21T02:48:26.981Z",
+        mrscore: 0,
+        updatedDate: "2021-02-21T02:48:26.981Z"
+    },
+    {
+        createdDate: "2021-02-21T02:48:26.981Z",
+        memberScore: 0,
+        mergeId: "Harry",
+        mergedDate: "2021-02-21T02:48:26.981Z",
+        mrscore: 0,
+        updatedDate: "2021-02-21T02:48:26.981Z"
+    },
+    {
+        createdDate: "2021-02-21T02:48:26.981Z",
+        memberScore: 0,
+        mergeId: "Louis",
+        mergedDate: "2021-02-21T02:48:26.981Z",
+        mrscore: 0,
+        updatedDate: "2021-02-21T02:48:26.981Z"
+    }
+]
