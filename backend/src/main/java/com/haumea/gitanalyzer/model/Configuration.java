723c9a7873f4558bc9b09df6f7dcecc51689d610
Austin Wang <ajwang@sfu.ca>
2021-03-07 17:38:31 -0800

    add Configuration for User

---
+++ backend/src/main/java/com/haumea/gitanalyzer/model/Configuration.java
@@ -0,0 +1,82 @@
+package com.haumea.gitanalyzer.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.HashMap;
+
+public class Configuration {
+
+    @Id
+    private String id;
+    private Date start;
+    private Date end;
+    private String gitlabServer;
+    private String targetBranch;
+    private HashMap<String, Float> editFactor;
+    private HashMap<String, Float> fileFactor;
+    private List<String> ignoreFileExtension;
+
+    // replace with commentType after it's merged
+    private String startType;
+    private String endType;
+
+
+    public Configuration() {
+        super();
+    }
+
+    @PersistenceConstructor
+    public Configuration(Date start, Date end, String gitlabServer,
+                         String targetBranch, HashMap<String, Float> editFactor,
+                         HashMap<String, Float> fileFactor, List<String> ignoreFileExtension,
+                         String startType, String endType) {
+
+        this.start = start;
+        this.end = end;
+        this.gitlabServer = gitlabServer;
+        this.editFactor = editFactor;
+        this.fileFactor = fileFactor;
+        this.ignoreFileExtension = ignoreFileExtension;
+        this.startType = startType;
+        this.endType = endType;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public String getGitlabServer() {
+        return gitlabServer;
+    }
+
+    public String getTargetBranch() {
+        return targetBranch;
+    }
+
+    public HashMap<String, Float> getEditFactor(){
+        return editFactor;
+    }
+
+    public HashMap<String, Float> getFileFactor(){
+        return fileFactor;
+    }
+
+    public List<String> getIgnoreFileExtension(){
+        return ignoreFileExtension;
+    }
+
+    public String getStartType() {
+        return startType;
+    }
+
+    public String getEndType() {
+        return endType;
+    }
+}
