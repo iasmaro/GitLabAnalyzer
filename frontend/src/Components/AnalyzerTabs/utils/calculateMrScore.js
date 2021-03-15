34110a5fee87384acdc62d9ad01b1f8ae12d429f
Ivan <ivan_asmaro@sfu.ca>
2021-03-14 22:44:41 -0700

    Remove log statement

--- frontend/src/Components/AnalyzerTabs/utils/calculateMrScore.js
+++ frontend/src/Components/AnalyzerTabs/utils/calculateMrScore.js
@@ -2,7 +2,6 @@ const calculateCommitScore = (mergerequests) => {
     let sumOfMRs = 0;
     for (let mergeRequest of mergerequests) {
         sumOfMRs += mergeRequest?.mrscore;
-        console.log(mergerequests);
     }
     sumOfMRs = Math.round(sumOfMRs * 10) / 10;
     return sumOfMRs;
