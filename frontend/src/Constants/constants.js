1a1b3ee60124a1c813de6300d370974b7dbe1f76
Neil Shah <nmshah@sfu.ca>
2021-03-14 22:00:45 -0700

    Updating constants

--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -1,27 +1,3 @@
-// const dev = {
-//     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://localhost:3000/',
-//     AUTHENTICATION_API_URL: 'http://localhost:8080/api/v1/users/userId?url=http://localhost:3000/',
-//     REPOS_API_URL: 'http://localhost:8080/api/v1/projects',
-//     MR_API_URL: 'http://localhost:8080/api/v1/mrs',
-//     MR_COMMITS_API_URL: 'http://localhost:8080/api/v1/commits/mergeRequests/',
-//     COMMITS_API_URL: 'http://localhost:8080/api/v1/commits/members/',
-//     USERS_API_URL: 'http://localhost:8080/api/v1/users',
-//     PROJECT_MEMBERS_API_URL: 'http://localhost:8080/api/v1/members',
-//     SAVE_CONFIG_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/configuration'
-// }
-
-// const prod = {
-//     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://cmpt373-1211-11.cmpt.sfu.ca/',
-//     AUTHENTICATION_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/userId?url=http://cmpt373-1211-11.cmpt.sfu.ca/',
-//     REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/projects',
-//     MR_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/mrs',
-//     MR_COMMITS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/commits/mergeRequests/',
-//     COMMITS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/commits/members/',
-//     USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users',
-//     PROJECT_MEMBERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/members',
-//     SAVE_CONFIG_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/configuration'
-// }
-
 const dev = {
     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://localhost:3000/',
     AUTHENTICATION_API_URL: 'http://localhost:8080/api/v1/users/userId?url=http://localhost:3000/',
