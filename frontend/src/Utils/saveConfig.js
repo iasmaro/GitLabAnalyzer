9b4e31fc543e2db2f79b562ce0821cef4f14f4bf
Neil Shah <nmshah@sfu.ca>
2021-03-14 21:04:19 -0700

    Made changes to post request

--- frontend/src/Utils/saveConfig.js
+++ frontend/src/Utils/saveConfig.js
@@ -1,7 +1,23 @@
 import { config } from 'Constants/constants';
 
 const saveConfig = async (languages, ConfigFileWeights, username, startDate, endDate, langaugeWeights, configName) => {
-    const URL = `${config.SAVE_CONFIG_URL}`;
+    const URL = `${config.SAVE_CONFIG_URL}?userId=${username}`;
+    let temp = JSON.stringify({
+        commentTypes: languages,
+        editFactor: ConfigFileWeights,
+        userId: username,
+        start: startDate,
+        end: endDate,
+        fileFactor: langaugeWeights,
+        fileName: configName,
+        ignoreFileExtension: [
+            'txt',
+            'md',
+        ],
+        targetBranch: 'master'
+    })
+    console.log(temp)
+
     fetch(URL, { 
         method: "POST",
         headers: { "Content-type": "application/json"},
