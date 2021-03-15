30081d53801357ddf0ddf87266efd3a2a006c48b
Neil Shah <nmshah@sfu.ca>
2021-03-14 22:35:25 -0700

    Removed unused variable

--- frontend/src/Components/Configurations/ConfigurationForm/ConfigForm.js
+++ frontend/src/Components/Configurations/ConfigurationForm/ConfigForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Form, Col, Row, Container, Button, Table } from 'react-bootstrap'
 
 import {ConfigLabels, initialConfigState} from 'Constants/constants'
