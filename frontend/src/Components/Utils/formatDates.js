769dd7c980a50ff3ba2527cb3b909634265535e8
Yi Ching Chang <changyic@sfu.ca>
2021-03-15 16:35:52 +0800

    Add missing space in date format

--- frontend/src/Components/Utils/formatDates.js
+++ frontend/src/Components/Utils/formatDates.js
@@ -3,7 +3,7 @@ export function utcToLocal(utcString) {
     if (!isNaN(localDate.getTime())) {
         return ('00' + (localDate.getMonth() + 1)).slice(-2) + '/' +
             ('00' + localDate.getDate()).slice(-2) + '/' +
-            localDate.getFullYear() + 'at' +
+            localDate.getFullYear() + ' at ' +
             ('00' + localDate.getHours()).slice(-2) + ':' +
             ('00' + localDate.getMinutes()).slice(-2);
     }
