f842dfe3bf7dafcca91c8dce2c1d4c1691d9549c
Sterling Tamboline <stamboli@sfu.ca>
2021-03-14 21:04:31 -0700

    Reverted changes to constants file

--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -1,15 +1,15 @@
 const dev = {
     SFU_LOGIN_URL: 'https://cas.sfu.ca/cas/login?service=http://localhost:3000/',
     AUTHENTICATION_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/userId?url=http://localhost:3000/',
-    REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/projects',
-    MR_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/mrs',
-    MR_COMMITS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/commits/mergeRequests/',
-    COMMITS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/commits/members/',
-    USERS_TOKEN_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/token',
-    USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users',
-    PROJECT_MEMBERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/members',
-    CONFIGURATION_FILES_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/configuration',
-    CONFIGURATION_FILE_INFO_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/configuration/',
+    REPOS_API_URL: 'http://localhost:8080/api/v1/projects',
+    MR_API_URL: 'http://localhost:8080/api/v1/mergeRequests/member/',
+    MR_COMMITS_API_URL: 'http://localhost:8080/api/v1/commits/mergeRequests/',
+    COMMITS_API_URL: 'http://localhost:8080/api/v1/commits/members/',
+    USERS_TOKEN_API_URL: 'http://localhost:8080/api/v1/users/token',
+    USERS_API_URL: 'http://localhost:8080/api/v1/users',
+    PROJECT_MEMBERS_API_URL: 'http://localhost:8080/api/v1/members',
+    CONFIGURATION_FILES_URL: 'http://localhost:8080/api/v1/users/configuration',
+    CONFIGURATION_FILE_INFO_URL: 'http://localhost:8080/api/v1/users/configuration/',
 }
 
 const prod = {
@@ -17,6 +17,7 @@ const prod = {
     AUTHENTICATION_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/userId?url=http://cmpt373-1211-11.cmpt.sfu.ca/',
     REPOS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/projects',
     MR_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/mergeRequests/member/',
+    MR_COMMITS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/commits/mergeRequests/',
     COMMITS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/commits/members/',
     USERS_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users',
     USERS_TOKEN_API_URL: 'http://cmpt373-1211-11.cmpt.sfu.ca:8080/api/v1/users/token',
