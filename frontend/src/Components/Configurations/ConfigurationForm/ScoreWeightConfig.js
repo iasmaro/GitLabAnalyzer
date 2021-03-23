76836a3497cc6d528632c6109d318578b718df73
Neil Shah <nmshah@sfu.ca>
2021-03-23 01:06:04 -0700

    Creating file to store input form for score weights

---
+++ frontend/src/Components/Configurations/ConfigurationForm/ScoreWeightConfig.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+import {ConfigLabels} from 'Constants/constants';
+
+import './ConfigForm.css';
+
+
+const ScoreWeightConfig = (props) => {
+    const { handleInputChange, state } = props || {};
+
+    return (
+        <tr>
+            <td>
+                <Form.Control 
+                    placeholder={ConfigLabels.SCORE_WEIGHT}  
+                    defaultValue={state.ADD_NEW_LINE}
+                    name = "ADD_NEW_LINE"
+                    onChange = {handleInputChange}
+                />
+            </td>
+            <td>
+                <Form.Control 
+                    placeholder={ConfigLabels.SCORE_WEIGHT}  
+                    defaultValue={state.DELETE_LINE}
+                    name = "DELETE_LINE"
+                    onChange = {handleInputChange}
+                />
+            </td>
+            <td>
+                <Form.Control 
+                    placeholder={ConfigLabels.SCORE_WEIGHT} 
+                    defaultValue={state.MOVE_LINE}
+                    name = "MOVE_LINE"
+                    onChange = {handleInputChange}
+                />
+            </td>
+            <td>
+                <Form.Control 
+                    placeholder={ConfigLabels.SCORE_WEIGHT}
+                    defaultValue={state.SPACING}
+                    name = "SPACING"
+                    onChange = {handleInputChange}
+                />
+            </td>
+            <td>
+                <Form.Control 
+                    placeholder={ConfigLabels.SCORE_WEIGHT} 
+                    defaultValue={state.SYNTAX}
+                    name = "SYNTAX"
+                    onChange = {handleInputChange}
+                />
+            </td>
+        </tr>
+    );
+};
+
+export default ScoreWeightConfig
