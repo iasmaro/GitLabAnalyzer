f25089b94373baf71821644f42595cdcf04166f1
Yi Ching Chang <changyic@sfu.ca>
2021-03-28 22:08:06 +0800

    Set start/end dates as current time if user has no set start/end

--- frontend/src/Components/RepoList/Repo.js
+++ frontend/src/Components/RepoList/Repo.js
@@ -19,8 +19,8 @@ const Repo = (props) => {
     const [members, setMembers] = useState([]);
     const [aliases, setAliases] = useState([]);
     const [databaseMapping, setDatabaseMapping] = useState([]);
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
     const username = useUserState();
     
     const handleShow = () => {
