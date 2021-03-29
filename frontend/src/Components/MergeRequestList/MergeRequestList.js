7a74d29162ebf138b0e63b942511bac5c0fe15cd
Sterling Tamboline <stamboli@sfu.ca>
2021-03-28 23:56:51 -0700

    Added sort functionality to merge request table

--- frontend/src/Components/MergeRequestList/MergeRequestList.js
+++ frontend/src/Components/MergeRequestList/MergeRequestList.js
@@ -5,10 +5,12 @@ import { message } from 'Constants/constants';
 
 import './MergeRequestList.css';
 import MergeRequest from './MergeRequest';
+import { useSortableDataObject, getClassNamesFor } from 'Utils/sortTables';
 
 
 const MergeRequestList = (props) => {
     const { mergerequests, setCommit, setCodeDiffs } = props || {};
+    const { items, requestSortObject, sortConfig  } = useSortableDataObject(mergerequests);
     const handleClick = (commits, diffs) => {
         if(setCodeDiffs) {
             setCodeDiffs(diffs);
@@ -27,20 +29,22 @@ const MergeRequestList = (props) => {
                     </thead>
                     <thead>
                         <tr className='mr-headers'>
-                            <th>Date Merged</th>
-                            <th>Title</th>
-                            <th>MR Score</th>
-                            <th>Commits Score</th>
-                            <th># Commits</th>
-                            <th colSpan='2'>Lines Changed</th>
+                            <th className={getClassNamesFor(sortConfig, 'mergedDate')} onClick={() => requestSortObject('mergedDate')}>Date Merged</th>
+                            <th className={getClassNamesFor(sortConfig, 'mergeRequestTitle')} onClick={() => requestSortObject('mergeRequestTitle')}>Title</th>
+                            <th className={getClassNamesFor(sortConfig, 'mrscore')} onClick={() => requestSortObject('mrscore')}>MR Score</th>
+                            <th className={getClassNamesFor(sortConfig, 'sumOfCommitScore')} onClick={() => requestSortObject('sumOfCommitScore')}>Commits Score</th>
+                            <th className={getClassNamesFor(sortConfig, 'commitDTOList')} onClick={() => requestSortObject('commitDTOList')}># Commits</th>
+                            <th className={getClassNamesFor(sortConfig, 'linesAdded')} onClick={() => requestSortObject('linesAdded')}>Lines +</th>
+                            <th className={getClassNamesFor(sortConfig, 'linesRemoved')} onClick={() => requestSortObject('linesRemoved')}>Lines -</th>
                         </tr>
                     </thead>
+                    {console.log(mergerequests)}
                     <tbody>
                         {!mergerequests?.length ? (
                             <td colSpan='8' >{message.NO_MERGE_REQUEST}</td>
                         )
                         :
-                        mergerequests.map((mergerequest) => (
+                        items.map((mergerequest) => (
                             <MergeRequest key={mergerequest?.mergeId} mergerequest={mergerequest} handleClick={handleClick} />
                         ))}
 
