a0e41be1ee6f45f3cc0ff86764c4378b655879d2
Viet <hvtruong@sfu.ca>
2021-04-02 22:07:54 -0700

    Adding GraphDTOs to Report

--- backend/src/main/java/com/haumea/gitanalyzer/dto/ReportDTO.java
+++ backend/src/main/java/com/haumea/gitanalyzer/dto/ReportDTO.java
@@ -5,22 +5,45 @@ import java.util.Map;
 
 public class ReportDTO {
 
+    int projectId;
     private Map<String, List<MergeRequestDTO>> mergeRequestListByMemberId;
     private Map<String, List<CommitDTO>> commitListByMemberId;
+
     private Map<String, List<CommentDTO>> MRCommentListByMemberId;
     private Map<String, List<CommentDTO>> issueCommentListByMemberId;
+
+    private Map<String, List<CommitGraphDTO>> commitGraphListByMemberId;
+    private Map<String, List<MergeRequestGraphDTO>> MRGraphListByMemberId;
+    private Map<String, List<CodeReviewGraphDTO>> codeReviewGraphListByMemberId;
+    private Map<String, List<IssueGraphDTO>> issueGraphListByMemberId;
+
+
     private List<String> userList;
 
-    public ReportDTO(Map<String, List<MergeRequestDTO>> mergeRequestListByMemberId,
+    public ReportDTO(int projectId,
+                     Map<String, List<MergeRequestDTO>> mergeRequestListByMemberId,
                      Map<String, List<CommitDTO>> commitListByMemberId,
                      Map<String, List<CommentDTO>> MRCommentListByMemberId,
                      Map<String, List<CommentDTO>> issueCommentListByMemberId,
+                     Map<String, List<CommitGraphDTO>> commitGraphListByMemberId,
+                     Map<String, List<MergeRequestGraphDTO>> MRGraphListByMemberId,
+                     Map<String, List<CodeReviewGraphDTO>> codeReviewGraphListByMemberId,
+                     Map<String, List<IssueGraphDTO>> issueGraphListByMemberId,
                      List<String> userList) {
+        this.projectId = projectId;
         this.mergeRequestListByMemberId = mergeRequestListByMemberId;
         this.commitListByMemberId = commitListByMemberId;
         this.MRCommentListByMemberId = MRCommentListByMemberId;
         this.issueCommentListByMemberId = issueCommentListByMemberId;
+        this.commitGraphListByMemberId = commitGraphListByMemberId;
+        this.MRGraphListByMemberId = MRGraphListByMemberId;
+        this.codeReviewGraphListByMemberId = codeReviewGraphListByMemberId;
+        this.issueGraphListByMemberId = issueGraphListByMemberId;
         this.userList = userList;
+    }
+
+    public int getProjectId() {
+        return projectId;
     }
 
     public Map<String, List<MergeRequestDTO>> getMergeRequestListByMemberId() {
@@ -37,6 +60,22 @@ public class ReportDTO {
 
     public Map<String, List<CommentDTO>> getIssueCommentListByMemberId() {
         return issueCommentListByMemberId;
+    }
+
+    public Map<String, List<CommitGraphDTO>> getCommitGraphListByMemberId() {
+        return commitGraphListByMemberId;
+    }
+
+    public Map<String, List<MergeRequestGraphDTO>> getMRGraphListByMemberId() {
+        return MRGraphListByMemberId;
+    }
+
+    public Map<String, List<CodeReviewGraphDTO>> getCodeReviewGraphListByMemberId() {
+        return codeReviewGraphListByMemberId;
+    }
+
+    public Map<String, List<IssueGraphDTO>> getIssueGraphListByMemberId() {
+        return issueGraphListByMemberId;
     }
 
     public List<String> getUserList() {
--- backend/src/main/java/com/haumea/gitanalyzer/service/ReportService.java
+++ backend/src/main/java/com/haumea/gitanalyzer/service/ReportService.java
@@ -1,9 +1,6 @@
 package com.haumea.gitanalyzer.service;
 
-import com.haumea.gitanalyzer.dto.CommentDTO;
-import com.haumea.gitanalyzer.dto.CommitDTO;
-import com.haumea.gitanalyzer.dto.MergeRequestDTO;
-import com.haumea.gitanalyzer.dto.ReportDTO;
+import com.haumea.gitanalyzer.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +15,16 @@ public class ReportService {
     private final MergeRequestService mergeRequestService;
     private final CommitService commitService;
     private final CommentService commentService;
+    private final GraphService graphService;
 
     private final MemberService memberService;
 
     @Autowired
-    public ReportService(MergeRequestService mergeRequestService, CommitService commitService, CommentService commentService, MemberService memberService) {
+    public ReportService(MergeRequestService mergeRequestService, CommitService commitService, CommentService commentService, GraphService graphService, MemberService memberService) {
         this.mergeRequestService = mergeRequestService;
         this.commitService = commitService;
         this.commentService = commentService;
+        this.graphService = graphService;
         this.memberService = memberService;
     }
 
@@ -33,8 +32,15 @@ public class ReportService {
 
         Map<String, List<MergeRequestDTO>> mergeRequestListByMemberId = new HashMap<>();
         Map<String, List<CommitDTO>> commitListByMemberId = new HashMap<>();
+
         Map<String, List<CommentDTO>> MRCommentListByMemberId = new HashMap<>();
         Map<String, List<CommentDTO>> issueCommentListByMemberId = new HashMap<>();
+
+        Map<String, List<CommitGraphDTO>> commitGraphListByMemberId = new HashMap<>();
+        Map<String, List<MergeRequestGraphDTO>> MRGraphListByMemberId = new HashMap<>();
+        Map<String, List<CodeReviewGraphDTO>> codeReviewGraphListByMemberId = new HashMap<>();
+        Map<String, List<IssueGraphDTO>> issueGraphListByMemberId = new HashMap<>();
+
         List<String> userList = new ArrayList<>();
 
         userList.add(userId);
@@ -45,21 +51,38 @@ public class ReportService {
 
             List<MergeRequestDTO> mergeRequestDTOs = mergeRequestService.getAllMergeRequestsForMember(userId, projectId, member);
             List<CommitDTO> commits = commitService.getCommitsForSelectedMemberAndDate(userId, projectId, member);
+
             List<CommentDTO> MRComments = commentService.getMergeRequestComments(userId, projectId, member);
             List<CommentDTO> issueComments = commentService.getMergeRequestComments(userId, projectId, member);
 
+            List<CommitGraphDTO> commitGraphs = graphService.getCommitGraphDetails(userId, member, projectId);
+            List<MergeRequestGraphDTO> MRGraphs = graphService.getMergeRequestGraphDetails(userId, member, projectId);
+            List<CodeReviewGraphDTO> codeReviewGraphs = graphService.getCodeReviewGraphDetails(userId, member, projectId);
+            List<IssueGraphDTO> issueGraphs = graphService.getIssueGraphDetails(userId, member, projectId);
+
             mergeRequestListByMemberId.put(member, mergeRequestDTOs);
             commitListByMemberId.put(member, commits);
+
             MRCommentListByMemberId.put(member, MRComments);
             issueCommentListByMemberId.put(member, issueComments);
+
+            commitGraphListByMemberId.put(member, commitGraphs);
+            MRGraphListByMemberId.put(member, MRGraphs);
+            codeReviewGraphListByMemberId.put(member, codeReviewGraphs);
+            issueGraphListByMemberId.put(member, issueGraphs);
 
         }
 
         return new ReportDTO(
+                projectId,
                 mergeRequestListByMemberId,
                 commitListByMemberId,
                 MRCommentListByMemberId,
                 issueCommentListByMemberId,
+                commitGraphListByMemberId,
+                MRGraphListByMemberId,
+                codeReviewGraphListByMemberId,
+                issueGraphListByMemberId,
                 userList);
 
     }
