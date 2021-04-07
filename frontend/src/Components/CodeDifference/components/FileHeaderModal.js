98dcfd72b8e65c0884d9b1be293fdd14353a7a12
Neil Shah <nmshah@sfu.ca>
2021-04-07 03:28:08 -0700

    Changing props and modifying modal

--- frontend/src/Components/CodeDifference/components/FileHeaderModal.js
+++ frontend/src/Components/CodeDifference/components/FileHeaderModal.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useUserState } from 'UserContext';
 import { Modal, Button } from 'react-bootstrap';
+
+import CodeDiffScoreBreakdown from './CodeDiffScoreBreakdown';
 
 const FileHeaderModal = (prop) => {
 
-    const {status, toggleModal, fileName} = prop || {};
-   
+    const { status, toggleModal, fileName, extension, linesMoved, addLine, syntaxLinesAdded, deleteLine, configInfo, spaceLinesAdded } = prop || {};
+
     return (
         <>
             <Modal
@@ -15,13 +16,22 @@ const FileHeaderModal = (prop) => {
                 keyboard={false}
                 size="xl"
                 className="custom-modal"
+                scrollable={true}       
             >
                 <Modal.Header closeButton>
                     <Modal.Title>{fileName} Score Breakdown</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body className="repo-modal-body">
-                    TEMP
+                    <CodeDiffScoreBreakdown 
+                        extension={extension}
+                        linesMoved={linesMoved}
+                        addLine={addLine}
+                        syntaxLinesAdded={syntaxLinesAdded}
+                        deleteLine={deleteLine}
+                        configInfo={configInfo}
+                        spaceLinesAdded={spaceLinesAdded}
+                    />
                 </Modal.Body>
 
                 <Modal.Footer>
