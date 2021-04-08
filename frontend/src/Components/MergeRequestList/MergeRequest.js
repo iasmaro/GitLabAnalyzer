71b99074ea7f2ff1e76e31565dbf38b13599d400
Yi Ching Chang <changyic@sfu.ca>
2021-04-09 06:42:56 +0800

    Remove unneccesary null checks

--- frontend/src/Components/MergeRequestList/MergeRequest.js
+++ frontend/src/Components/MergeRequestList/MergeRequest.js
@@ -7,16 +7,21 @@ import './MergeRequestList.css';
 
 const MergeRequest = (props) => {
     const { mergerequest, handleClick, selected, index } = props || {};
+
+    if (!mergerequest) {
+        return null;
+    }
+
     const mergeRequestRowClass = selected ? 'merge-request-selected' : 'merge-request';
     const dummyTooltip = <Tooltip>
         Dummy refers to all the commits that were made directly to the master. These commits have no associated merge requests, hence MR score is not applicable.
         </Tooltip>;
-    const mergeRequestTitleDisplay = mergerequest?.mergeRequestTitle?.length > 40 ? mergerequest?.mergeRequestTitle?.slice(0, 40) + '...' : mergerequest?.mergeRequestTitle?.slice(0, 40);
+    const mergeRequestTitleDisplay = mergerequest.mergeRequestTitle?.length > 40 ? mergerequest.mergeRequestTitle.slice(0, 40) + '...' : mergerequest.mergeRequestTitle.slice(0, 40);
 
     return (
-        <tr className={mergeRequestRowClass} onClick={() => {handleClick(mergerequest?.commitDTOList, mergerequest?.mergeRequestDiffs, index, mergerequest?.mergeRequestTitle)}} >
+        <tr className={mergeRequestRowClass} onClick={() => {handleClick(mergerequest.commitDTOList, mergerequest.mergeRequestDiffs, index, mergerequest.mergeRequestTitle)}} >
             <td>
-                {mergerequest?.mergeId !== -1 ? utcToLocal(mergerequest?.mergedDate) : 
+                {mergerequest.mergeId !== -1 ? utcToLocal(mergerequest.mergedDate) : 
                     <OverlayTrigger 
                         placement='bottom' 
                         overlay={dummyTooltip}>
@@ -29,23 +34,23 @@ const MergeRequest = (props) => {
                 }
             </td>
             <td>
-                {mergerequest?.mergeId === -1 ? mergerequest?.mergeRequestTitle :
+                {mergerequest.mergeId === -1 ? mergerequest.mergeRequestTitle :
                     <OverlayTrigger
                         placement='top'
                         overlay={
                             <Tooltip className='tooltip'>
-                                {mergerequest?.mergeRequestLink}
+                                {mergerequest.mergeRequestLink}
                             </Tooltip>
                         }
                     >
-                        <a href={mergerequest?.mergeRequestLink} target='_blank' rel='noreferrer'>{mergeRequestTitleDisplay}</a>
+                        <a href={mergerequest.mergeRequestLink} target='_blank' rel='noreferrer'>{mergeRequestTitleDisplay}</a>
                     </OverlayTrigger>}
             </td>
-            <td>{mergerequest?.mergeId === -1 ? 'N/A' : mergerequest?.mrscore}</td>
-            <td>{mergerequest?.sumOfCommitScore}</td>
-            <td>{mergerequest?.commitDTOList?.length}</td>
-            <td className='lines-added'>{mergerequest?.mergeId === -1 ? 'N/A' : ['+', mergerequest?.linesAdded].join('')}</td>
-            <td className='lines-removed'>{mergerequest?.mergeId === -1 ? 'N/A' : ['-', mergerequest?.linesRemoved].join('')}</td>
+            <td>{mergerequest.mergeId === -1 ? 'N/A' : mergerequest.mrscore}</td>
+            <td>{mergerequest.sumOfCommitScore}</td>
+            <td>{mergerequest.commitDTOList.length}</td>
+            <td className='lines-added'>{mergerequest.mergeId === -1 ? 'N/A' : ['+', mergerequest.linesAdded].join('')}</td>
+            <td className='lines-removed'>{mergerequest.mergeId === -1 ? 'N/A' : ['-', mergerequest.linesRemoved].join('')}</td>
         </tr>
     );
 };
