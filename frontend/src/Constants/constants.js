b7d662cda05999a949b19e306519eccaf10eabbd
Neil Shah <nmshah@sfu.ca>
2021-04-09 16:01:33 -0700

    Reverted changes in constant

--- frontend/src/Constants/constants.js
+++ frontend/src/Constants/constants.js
@@ -12,7 +12,7 @@ const dev = {
     CONFIGURATION_FILES_URL: 'http://localhost:8080/api/v1/users/configuration',
     CONFIGURATION_FILE_INFO_URL: 'http://localhost:8080/api/v1/users/configuration/',
     START_URL: 'http://localhost:8080/api/v1/users/start/',
-    END_URL: 'http://localhost:8080/api/v1/users/end/',
+    END_URL: 'http://localhost:8080/api/v1/users/end/'
 };
 
 const prod = {
