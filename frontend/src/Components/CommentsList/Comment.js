63f485db6cac14f4945e72587972fa62613da69f
Sterling Tamboline <stamboli@sfu.ca>
2021-04-10 10:22:46 -0700

    Moved conditional ternary operator onto separate lines

--- frontend/src/Components/CommentsList/Comment.js
+++ frontend/src/Components/CommentsList/Comment.js
@@ -8,22 +8,27 @@ const Comment = (props) => {
     const { creationDate, onOwnRequestOrIssue, title, url, commentDescription, isMR } = props || {};
     return (
         <tr className='comment-row'>
-            {isMR ? <td className='is-own'>{onOwnRequestOrIssue ? ("Own MR") : ("Other's MR")}</td> : <td className='is-own'>{onOwnRequestOrIssue ? ("Own Issue") : ("Other's Issue")}</td>}
+            {isMR ? 
+                <td className='is-own'>{onOwnRequestOrIssue ? ("Own MR") : ("Other's MR")}</td> : 
+                <td className='is-own'>{onOwnRequestOrIssue ? ("Own Issue") : ("Other's Issue")}</td>
+            }
             <td className='creation-date'>{utcToLocal(creationDate)}</td>
             <td className='title'>
                 <a href={url} target='_blank' rel='noreferrer'>{title}</a>
             </td>
-            <td className='comment-description'>{commentDescription.length < 100 ? commentDescription : 
-                (<OverlayTrigger
-                    placement="top"
-                    overlay={
-                        <Tooltip className='tooltip'>
-                            {commentDescription}
-                        </Tooltip>
-                    }
-                >
-                    <p>{commentDescription.slice(0, 100) + '...'}</p>
-                </OverlayTrigger>)}
+            <td className='comment-description'>
+                {commentDescription.length < 100 ? commentDescription : 
+                    <OverlayTrigger
+                        placement="top"
+                        overlay={
+                            <Tooltip className='tooltip'>
+                                {commentDescription}
+                            </Tooltip>
+                        }
+                    >
+                        <p>{commentDescription.slice(0, 100) + '...'}</p>
+                    </OverlayTrigger>
+                }
             </td>
             <td className='word-count'>{commentDescription.match(/(\w+)/g).length}</td>
         </tr>
