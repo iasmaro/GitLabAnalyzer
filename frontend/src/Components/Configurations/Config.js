fde34cb7e189a8c6c305c1932cedd1463fb002ee
Sterling Tamboline <stamboli@sfu.ca>
2021-04-11 18:36:09 -0700

    Added max width to config names so that they do not push buttons out of view

--- frontend/src/Components/Configurations/Config.js
+++ frontend/src/Components/Configurations/Config.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { FiEdit } from 'react-icons/fi';
-
+import './Config.css';
 
 const Config = (props) => {
     const { config, handleClick, handleDelete, handleEdit } = props || {};
     
     return (
         <tr>
-            <td onClick={() => {handleClick && handleClick(config)}}>{config}</td>
+            <td className='config-name' onClick={() => {handleClick && handleClick(config)}}>{config}</td>
             <td className="config-button"><Button className="edit-config-btn" variant="warning" onClick={() => handleEdit && handleEdit(config)}><FiEdit /></Button></td>
             <td className="config-button"><Button variant="danger" className="delete-config-button" onClick={() => handleDelete && handleDelete(config)}><AiOutlineDelete /></Button></td>
         </tr>
